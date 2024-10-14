d17a08e7e75596cb1c7ae25b6f659e67faf797ac
Ketan Khot <deltacodingketan@gmail.com>
2024-10-14 14:37:26 +0530

    Create Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import SignIn from "../src/pages/SignIn";
+import SignUp from "../src/pages/SignUp";
+import Dashboard from "../src/pages/Dashboard";
+import Projects from "../src/pages/Projects";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
