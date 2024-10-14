f03fa1942c725ff3bef5f4da58038ac10c4ec2ec
Ketan Khot <deltacodingketan@gmail.com>
2024-10-14 16:53:42 +0530

    MongoDB Connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
+dotenv.config();
+
+// Connections to Mongoose
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Failed to connect to MongoDB");
+    console.log(error);
+  });
 
 app.get("/", (req, res) => res.send("Blog Application...!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
