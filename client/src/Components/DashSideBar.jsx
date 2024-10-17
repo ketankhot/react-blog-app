c4b4f53fcac92c69c0e2223c8283f486428bbc56
Ketan Khot <deltacodingketan@gmail.com>
2024-10-17 12:01:00 +0530

    complete sidebar of the dashboard

---
+++ client/src/Components/DashSideBar.jsx
@@ -0,0 +1,40 @@
+import { Sidebar } from "flowbite-react";
+import { useState, useEffect } from "react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+
+const DashSideBar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    console.log(tabFormUrl);
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56 p-1">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to={"/dashboard?tab=profile"}>
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item className="cursor-pointer" icon={HiArrowSmRight}>
+            Sign out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSideBar;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,27 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSideBar from "../Components/DashSideBar";
+import DashProfile from "../Components/DashProfile";
 
-export default Dashboard;
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    console.log(tabFormUrl);
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="flex min-h-screen flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Side-Bar */}
+        <DashSideBar />
+      </div>
+      {/* Profile */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
+}
